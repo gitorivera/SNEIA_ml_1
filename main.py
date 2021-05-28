eef7bb05686c41388d01769061c9809e6d41e82b
Jorge Rivera <jorgerivera@Jorges-iMac.local>
2021-05-28 13:48:48 -0500

    El segundo commit ue hago

--- main.py
+++ main.py
@@ -7,3 +7,22 @@ import numpy as np
 # - lista con enteros usando la fincccón rnge
 int_list = list(range(10))
 print(int_list)
+
+# - armar una lista de caracteres iterando sobre una lista
+str_list= [str(i) for i in int_list]
+print(str_list)
+new_list = str_list*2
+# notese que al multiplica el vector x 2 solamente se conectan dos vectores iguales
+print(new_list)
+
+int_array = np.array(int_list)
+print(int_array)
+new_array = int_array*2
+print(new_array)
+
+# Adicionalmente el metodeo numpy trae:
+# ndim: el numero de dimensiones
+# shape: el tamaño de cada dimensión
+# size: El numero total de elementos en el arreglo
+# dtype: el tipo de dato en el array
+
